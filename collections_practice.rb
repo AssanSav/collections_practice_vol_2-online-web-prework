695a4db2595fee04d74cd82785d97743b4b9f729
assane savadogo <assane.savadogo81@gmail.com>
2019-06-12 04:33:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,7 +37,7 @@ def organize_schools(array)
 org_school = {}
 array.each do |key, value|
   if (org_school[value[:location]] == nil)
-    org_school[value[:location]] = key 
+    org_school[value[:location]] = [key]
   else
     org_school[value[:location]] << key
   end
