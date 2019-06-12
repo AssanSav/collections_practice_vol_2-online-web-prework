b015926eabb4b7e72c959b829a91f58fe64becad
assane savadogo <assane.savadogo81@gmail.com>
2019-06-12 03:41:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|element| element.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select {|element| element.include?('a')}
+end
+
+def first_wa(array)
+array.find {|element| element.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+array.grep(String)
+end
+
+def count_elements(array)
+array.uniq.collect do |element|
+  element[:count] = array.count(element)
+  element
+   end
+end
+
+def merge_data(array)
+array.each {|data| data.merge}
+end
